e75751dd19235ca2dcbcefde2bb2bf220dd3baaf
vlakomov <vlakomov@gmail.com>
2024-09-17 14:47:05 +0300

    added Quartz scheduler package

---
+++ Gm.Infrastructure/Configuration/DependencyInjection/SchedulerExtensions.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+
+namespace Gm.Infrastructure.Configuration.DependencyInjection;
+
+public static class SchedulerExtensions
+{
+    public static IServiceCollection ConfigureScheduler(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddQuartz();
+        services.AddQuartzHostedService(options =>
+        {
+            options.StartDelay = TimeSpan.FromSeconds(5);
+            options.AwaitApplicationStarted = true;
+            options.WaitForJobsToComplete = true;
+        });
+        
+        return services;
+    }
+    
+    
+}
---
+++ Gm.Infrastructure/Scheduler/Jobs/GmSenderJob.cs
@@ -0,0 +1,14 @@
+﻿using Gm.Infrastructure.TelegramBot.Abstract;
+using Quartz;
+
+namespace Gm.Infrastructure.Scheduler.Jobs;
+
+public class GmSenderJob(ISenderService senderService) : IJob
+{
+    public ISenderService SenderService { get; } = senderService;
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        await senderService.SendAsync(default);
+    }
+}
