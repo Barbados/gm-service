bd355092e3f6fca5f674da02b8233d6121fdec19
vlakomov <vlakomov@gmail.com>
2024-09-18 18:40:55 +0300

    added initial db migration

---
+++ Gm.Infrastructure/Data/Migrations/20240917194205_Initial.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Gm.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "subscribers",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    tg_chat_id = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_subscribers", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "subscriptions",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    subscription_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    subscription_schedule = table.Column<string>(type: "text", nullable: true),
+                    is_active = table.Column<bool>(type: "boolean", nullable: false),
+                    topic = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_subscriptions", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_subscriptions_subscribers_subscription_id",
+                        column: x => x.subscription_id,
+                        principalTable: "subscribers",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_subscriptions_subscription_id",
+                table: "subscriptions",
+                column: "subscription_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_subscriptions_topic",
+                table: "subscriptions",
+                column: "topic");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "subscriptions");
+
+            migrationBuilder.DropTable(
+                name: "subscribers");
+        }
+    }
+}
