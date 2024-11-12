dd2d777bdf27ae089526451928c9ecb8260e6c41
Barbados <vlacomov@gmail.com>
2024-11-12 16:54:58 +0300

    changed time of message posting

--- src/Gm.TgDaemon/Program.cs
+++ src/Gm.TgDaemon/Program.cs
@@ -1,5 +1,4 @@
-﻿using System.Globalization;
-using Gm.Infrastructure.Configuration.DependencyInjection;
+﻿using Gm.Infrastructure.Configuration.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder();
@@ -14,20 +13,4 @@ services.ConfigureScheduler(configuration);
 var host = builder.Build();
 host.InitializeDatabase();
 
-// TestDateCultureAsync();
-
 await host.RunAsync();
-
-void TestDateCultureAsync()
-{
-    Console.WriteLine(ComposeGmMessage());
-}
-
-string ComposeGmMessage()
-{
-    var culture = new CultureInfo("ru-RU");
-    var message = string.Create(culture, $"Доброе утро!\nСегодня {DateTime.Today:D}");
-    message += $"\nХорошего дня! Увидимся завтра.";
-
-    return message;
-}
